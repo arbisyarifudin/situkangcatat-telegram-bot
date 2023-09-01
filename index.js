08aebc0a7af1c4c46177eb96bd26f2747bcde523
Arbi Syarifudin <arbisyarifudin@gmail.com>
2023-09-01 15:32:54 +0700

    add save to CSV functionality

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+require('dotenv').config()
+
 const TelegramBot = require('node-telegram-bot-api')
-const fs = require('fs')
 const https = require('https')
+const fs = require('fs')
+
+// const baseURL = process.env.BASE_URL || 'http://localhost:3000'
 
 // Ganti dengan token bot Anda
-const token = '6263342264:AAGD2S5RRlPATHW10OKHKFDr0SDMLJ8Pg8g'
+const token = process.env.TELEGRAM_BOT_TOKEN
 
 // ID grup yang diizinkan
 const allowedGroup = [
   {
-    id: -1001812257489,
-    topicIds: [23]
+    id: -1001812257489, // ID grup di Telegram (ganti dengan ID grup Anda)
+    topicIds: [23] // ID topik di grup (ganti dengan ID topik Anda)
   }
 ]
 
@@ -18,7 +22,7 @@ const bot = new TelegramBot(token, { polling: true })
 
 // Mendengarkan pesan masuk
 bot.on('message', async (msg) => {
-  console.log('message', msg)
+//   console.log('message', msg)
 
   const messageId = msg.message_id
   const chatId = msg.chat.id
@@ -28,8 +32,6 @@ bot.on('message', async (msg) => {
   const isAllowedGroup = allowedGroup.some(group => {
     return group.id === chatId && group.topicIds.includes(threadId)
   })
-
-  console.log('isAllowedGroup', isAllowedGroup)
 
   if (!isAllowedGroup) return
 
@@ -41,7 +43,7 @@ bot.on('message', async (msg) => {
   }
 
   if (msgText) {
-    const validKeywords = ['absen masuk', 'absen keluar', 'masuk', 'keluar']
+    const validKeywords = ['masuk', 'pulang', 'izin', 'sakit', 'cuti']
 
     if (validKeywords.some(keyword => msgText.includes(keyword))) {
       if (msg.photo && msg.photo.length > 0) {
@@ -49,12 +51,43 @@ bot.on('message', async (msg) => {
         const photoId = msg.photo[msg.photo.length - 1].file_id
 
         try {
-          const photoPath = await downloadPhoto(photoId)
-          console.log('photoPath', photoPath)
           // Lakukan validasi tambahan jika diperlukan
 
+          // Unduh foto dari pesan
+          /* const photoPath =  */await downloadPhoto(photoId)
+          //   console.log('photoPath', photoPath)
+
+          // Ambil status dari pesan
+          const status = msgText.includes('izin') ? 'Izin' : msgText.includes('sakit') ? 'Sakit' : msgText.includes('cuti') ? 'Cuti' : msgText.includes('masuk') ? 'Masuk' : 'Pulang'
+
+          // Ambil tanggal dan waktu dari pesan
+          const date = new Date(msg.date * 1000) // Konversi dari UNIX Timestamp ke Date
+
+          // Buat tanggal dan waktu jadi GMT+7
+          const dateString = date.toLocaleDateString('id-ID')
+          const timeString = date.toLocaleTimeString('id-ID').replace(/\./g, ':')
+          const dayName = date.toLocaleDateString('id-ID', { weekday: 'long' })
+
+          // add leading zero to single digit
+          const dateTimeString = `${dateString} ${timeString}`.replace(/(?<!\d)(\d)(?!\d)/g, '0$1')
+
+          //   const photUrl = `${baseURL}/${photoPath}`
+
+          // Ambil username dari pesan
+          const username = `@${msg.from.username}`
+
+          // Ambil informasi dari msg untuk dimasukkan ke dalam file CSV
+          //   const rowData = [msg.from.username, status, dateString, photUrl]
+          const rowData = [username, status, msgText, dateTimeString]
+
+          const dateFileName = date.toISOString().split('T')[0] // format: YYYY-MM-DD
+          saveToCSV([rowData], `datas/attendance-${dateFileName}.csv`)
+
+          const successMessageText = `Kehadiran berhasil dicatat.\n\n<code>Username: ${username}\nStatus: ${status}\nTimestamp: ${dayName}, ${dateTimeString} WIB</code>`
+
           // Kirim pesan sukses
-          bot.sendMessage(chatId, 'Kehadiran berhasil dicatat.', {
+          bot.sendMessage(chatId, successMessageText, {
+            parse_mode: 'HTML',
             reply_to_message_id: messageId
           })
         } catch (error) {
@@ -68,7 +101,8 @@ bot.on('message', async (msg) => {
       }
     } else {
       // Kirim pesan error jika format kata kunci salah
-      bot.sendMessage(chatId, 'Error: Format pesan salah. Gunakan salah satu dari: Absen Masuk, Absen Keluar, Masuk, Keluar.', {
+      bot.sendMessage(chatId, `Error: Format kata kunci salah.\n\nGunakan: <code>${validKeywords.join(', ')}</code>`, {
+        parse_mode: 'HTML',
         reply_to_message_id: messageId
       })
     }
@@ -114,8 +148,53 @@ function downloadPhoto (fileId) {
       request.on('error', err => {
         reject(err)
       })
+
+      // Hapus folder yang bukan bulan ini
+      const currentMonth = date.getMonth()
+      const currentYear = date.getFullYear()
+      const currentMonthString = `${currentYear}-${currentMonth.toString().padStart(2, '0')}`
+      const downloadsPath = 'downloads'
+      const folders = fs.readdirSync(downloadsPath)
+      folders.forEach(folder => {
+        // check if folder has name like YYYY-MM
+        if (folder.match(/^\d{4}-\d{2}$/)) {
+          // get folder year and month
+          const folderYear = folder.split('-')[0]
+          const folderMonth = folder.split('-')[1]
+
+          // delete folder if not current year month
+          const folderYearMonthString = `${folderYear}-${folderMonth}`
+          if (folderYearMonthString !== currentMonthString) {
+            fs.rmdirSync(`${downloadsPath}/${folder}`, { recursive: true })
+          }
+        }
+      })
     }).catch(err => {
       reject(err)
     })
   })
+}
+
+const headerTitles = ['Nama', 'Status', 'Pesan', 'Tanggal']
+
+function saveToCSV (data, fileName, folder = 'datas') {
+  // Membuat direktori jika belum ada
+  if (!fs.existsSync(folder)) {
+    fs.mkdirSync(folder, { recursive: true })
+  }
+
+  // Membuat file CSV baru jika belum ada
+  if (!fs.existsSync(fileName)) {
+    fs.writeFileSync(fileName, headerTitles.join(',') + '\n', 'utf-8')
+  }
+
+  // Mengonversi data presensi ke dalam baris-baris CSV
+  const rows = data.map(row => {
+    return row.join(',')
+  })
+
+  // Menggabungkan baris-baris data dengan file CSV yang sudah ada
+  fs.appendFileSync(fileName, rows.join('\n') + '\n', 'utf-8')
+
+//   console.log(`Data presensi telah ditambahkan ke dalam file ${fileName}`)
 }
---
+++ readme.md
@@ -0,0 +1,19 @@
+# TUKANG CATATIN BOT (Telegram)
+
+## Cara Install
+1. Clone repository ini
+2. Buka terminal dan masuk ke direktori repository
+3. Jalankan `yarn install` atau `npm install`
+4. Buat file `.env` dan isi dengan:
+```
+TELEGRAM_BOT_TOKEN=
+```
+5. Jalankan bot dengan cara `yarn start` atau `npm start`
+
+## Cara Buat Bot Telegram
+1. Buka Telegram
+2. Cari bot dengan username `@botfather`
+3. Ketik `/newbot` dan ikuti petunjuknya
+4. Setelah bot berhasil dibuat, salin token yang diberikan
+5. Buka file `.env` dan isi `TELEGRAM_BOT_TOKEN` dengan token yang sudah disalin
+
